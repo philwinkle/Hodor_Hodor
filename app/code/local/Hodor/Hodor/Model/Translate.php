0cffcee4573678101e2d650a31fde98e9a9f63a7
Phillip Jackson <philwinkle@gmail.com>
2015-03-25 08:53:49 -0400

    Initial commit - first approach rewrite translation

---
+++ app/code/local/Hodor/Hodor/Model/Translate.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hodor_Hodor_Model_Translate extends Mage_Core_Model_Translate
+{
+    /**
+     * Translate
+     *
+     * @param   array $args
+     * @return  string
+     */
+	public function translate($args)
+	{
+		return 'Hodor ';
+	}
+}
